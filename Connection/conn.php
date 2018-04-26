799502ff1a77816b3354e4f32f07e81824bd7ed9
i7madh <36330198+i7madh@users.noreply.github.com>
2018-04-27 01:07:49 +0300

    Update conn.php

--- Connection/conn.php
+++ Connection/conn.php
@@ -1,5 +1,5 @@
 <?php
-$db = mysqli_connect ('127.0.0.1','root','','ezhalha') ;
+$db = mysqli_connect ('eu-cdbr-west-02.cleardb.net','b579b0d570201d','0288fa8e','heroku_033b3c3b3f6a2b6') ;
 
 if(mysqli_connect_errno()) {
   echo 'Database connection failed with following errors : '. mysqli_connect_error();
